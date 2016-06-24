0cd9704293e5382a2c44aefe74585fdf630eada8
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-06-24 21:58:14 +0300

    Add 50A problem solution

---
+++ 50A/50A/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  50A
+//
+//  Created by Dmitry Fa[n]tastik on 24/06/2016.
+//  Copyright © 2016 Fantastik. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int m, n;
+
+    cin >> m >> n;
+
+    cout << ((m * n) / 2) << endl;
+
+    return 0;
+}
